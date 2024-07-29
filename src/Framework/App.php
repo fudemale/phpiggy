6ed65ff959c372e07b7ad7bad69fdf960834aa33
fudemale <55007910+fudemale@users.noreply.github.com>
2024-07-29 13:07:54 +0500

    added routes

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class App {
+class App 
+{
+    private Router $router;
 
-    public function run(){
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+    public function run()
+    
+    {
 
-        echo "Application is running";
+       $path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+       $method = $_SERVER['REQUEST_METHOD'];
+       $this->router->dispath($path,$method);
+    }
+
+    public function get(string $path , array $controller)
+    // ^ was a add fn before changed to get as it's a get method
+    {
+        $this->router->add('GET',$path, $controller);
     }
 }
