1ce73ecc8c06e4245958dcdaf6f294c1e2435825
fudemale <55007910+fudemale@users.noreply.github.com>
2024-07-30 12:34:03 +0500

    using template engines as class for paths

---
+++ src/App/Config/Paths.php
@@ -0,0 +1,11 @@
+<?php
+// this file will contain the configuration for our php application
+declare(strict_types=1);
+
+namespace App\Config;
+
+
+class Paths
+{
+    public const VIEW = __DIR__ . "/../views";
+}
