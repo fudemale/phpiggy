98eb5dbe40a05bdd3eca0058af5c45549403e92b
fudemale <55007910+fudemale@users.noreply.github.com>
2024-08-12 04:17:23 +0500

    connected to mysql database

---
+++ cli.php
@@ -0,0 +1,36 @@
+<?php
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database('mysql', [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy',
+
+], 'root', '');
+
+$query = "SELECT * FROM products";
+
+$stmt = $db->connection->query($query, PDO::FETCH_ASSOC);
+
+var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+
+
+// $driver = 'mysql';
+
+// $config = http_build_query(data: [
+//     'host' => 'localhost',
+//     'port' => 3306,
+//     'dbname' => 'phpiggy'
+// ], arg_separator: ';');
+
+// $dsn = "{$driver}:{$config}";
+// $username = 'root';
+// $password = '';
+
+// try {
+//     $db = new PDO($dsn, $username, $password);
+// } catch (PDOException $e) {
+//     die("Unable to connect to database");
+// }
